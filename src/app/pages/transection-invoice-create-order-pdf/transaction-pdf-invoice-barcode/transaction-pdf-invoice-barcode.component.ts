9bb5e66d68855a7fec64b55e9b72127188c23cc7
Dipta Sikder <anotheramit8@gmail.com>
2026-04-29 21:12:53 +0600

    refactor: tidy up HTML and TypeScript formatting in barcode component

--- src/app/pages/transection-invoice-create-order-pdf/transaction-pdf-invoice-barcode/transaction-pdf-invoice-barcode.component.ts
+++ src/app/pages/transection-invoice-create-order-pdf/transaction-pdf-invoice-barcode/transaction-pdf-invoice-barcode.component.ts
@@ -2,12 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'transaction-pdf-invoice-barcode',
-    standalone: true,
-    imports: [CommonModule],
-    templateUrl: './transaction-pdf-invoice-barcode.component.html',
-    styleUrl: './transaction-pdf-invoice-barcode.component.scss',
+  selector: 'transaction-pdf-invoice-barcode',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transaction-pdf-invoice-barcode.component.html',
+  styleUrl: './transaction-pdf-invoice-barcode.component.scss',
 })
 export class TransactionPDFInvoiceBarcode {
-    @Input({ required: true }) barcodeUrl!: string;
+  @Input({ required: true }) barcodeUrl!: string;
 }
